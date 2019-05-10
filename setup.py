de7c6262450c19a9f37500bf16316cad564ace3a
Maria Öhrman <maria.ohrman@gu.se>
2019-05-10 13:58:10 +0200

    move to src folder

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     setup_requires=['pbr'],
-    pbr=True
+    pbr=True,
+    package_dir={'': 'src'},
+    packages=find_packages(where='src')
 )
 
