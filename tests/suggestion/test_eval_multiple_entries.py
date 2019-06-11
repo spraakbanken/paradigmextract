69fd8c748dea05b08928fe5b6e1198b3ea348754
Maria Öhrman <maria.ohrman@gu.se>
2019-06-11 17:06:41 +0200

    more tests

--- tests/suggestion/test_eval_multiple_entries.py
+++ tests/suggestion/test_eval_multiple_entries.py
@@ -1,7 +1,27 @@
-import pytest
 from morphparser import eval_multiple_entries
+from paradigm import Paradigm
 
 
-@pytest.mark.skip()
 def test_1():
-    eval_multiple_entries()
+    form_msds = [('1', ('msd', 'sg indef nom')), ('1+n', ('msd', 'pl def nom')), ('1+ns', ('msd', 'sg def gen'))]
+    var_insts = [[('1', 'apa')]]
+    p = Paradigm(form_msds, var_insts)
+    words = ['apa', 'apan', 'apans']
+    variables = eval_multiple_entries(p, words, baseform=True)
+    assert {('apa',)} == variables
+
+
+def test_2():
+    form_msds = [('apa', ('msd', 'sg indef nom')), ('bepa', ('msd', 'pl def nom'))]
+    p = Paradigm(form_msds, [[]])
+    words = ['apa']
+    variables = eval_multiple_entries(p, words, baseform=True)
+    assert {()} == variables
+
+
+def test_3():
+    form_msds = [('apa', ('msd', 'sg indef nom')), ('bepa', ('msd', 'pl def nom'))]
+    p = Paradigm(form_msds, [[]])
+    words = ['apa']
+    variables = eval_multiple_entries(p, words, baseform=False)
+    assert {()} == variables
