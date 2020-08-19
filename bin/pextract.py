7e7cc370859916c3b31871ab0f5e7b693cd98420
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-08-19 10:38:15 +0200

    refactor: remove py2-unicde strings

--- bin/pextract.py
+++ bin/pextract.py
@@ -5,50 +5,51 @@ import paradigmextract.pextract as pextract
 
 
 def split_tags(tags: List):
+    spl = [tg.split(",,") for tg in tags]
 
     newforms = []
     ctr = 1
     for form in spl:
         newelement = []
         for tagelement in form:
-            if tagelement == u'':
-                newelement.append((str(ctr), u'1'))
-            elif u'=' in tagelement:
-                splittag = tagelement.split(u'=')
+            if tagelement == "":
+                newelement.append((str(ctr), "1"))
+            elif "=" in tagelement:
+                splittag = tagelement.split("=")
                 newelement.append((splittag[0], splittag[1]))
             else:
-                newelement.append((tagelement, u'1'))
+                newelement.append((tagelement, "1"))
         newforms.append(newelement)
         ctr += 1
     return newforms
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     lines = [l.strip() for l in sys.stdin]
     tables = []
     thistable = []
     thesetags = []
     for l in lines:
-        if l == '':
-            if len(thistable) > 0:
+        if l == "":
+            if thistable:
                 splittags = split_tags(thesetags)
                 tables.append((thistable, splittags))
                 thistable = []
                 thesetags = []
         else:
-            if '\t' in l:
-                _, tag, form = l.split('\t')
+            if "\t" in l:
+                _, tag, form = l.split("\t")
             else:
                 form = l
-                tag = ''
+                tag = ""
             thistable.append(form)
             thesetags.append(tag)
 
-    if len(thistable) > 0:
+    if thistable:
         splittags = split_tags(thesetags)
         tables.append((thistable, splittags))
 
     learnedparadigms = pextract.learnparadigms(tables)
     for p in learnedparadigms:
         # print(str(p) + '\n\n')
-        print('a paradigm')
+        print("a paradigm")
