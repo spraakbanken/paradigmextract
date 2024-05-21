21133a0d1b40681aa00448c25e0fa7bed98c4f84
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-05-21 10:38:49 +0200

    test: update path to testdata.json

--- tests/integration_tests/test_all_learn_paradigms.py
+++ tests/integration_tests/test_all_learn_paradigms.py
@@ -1,23 +1,20 @@
 import json
-import os
+from pathlib import Path
 
 from paradigmextract.pextract import learnparadigms
 
 
 def test_all():
-    join = os.path.join("/", *os.path.realpath(__file__).split("/")[:-2], "testdata.json")
-    with open(join) as fp:
+    with Path("tests/testdata.json").open() as fp:
         test_tables = json.load(fp)
-        final_tables = []
-        assert len(test_tables) == 9943
-        for table in test_tables:
-            msds_ = table["msds"]
-            tags = []
-            for msd in msds_:
-                tags.append(("msd", msd))
-            final_tables.append((table["wordforms"], tags))
-        new_paradigms = learnparadigms(final_tables)
-        # this value is transient, sometimes 551 sometimes 552 (for full testset)
-        # it differs for the words "bortgallra", "bortkollra", "bortjaga", "bortsopa"
-        # where they sometimes are all in the same paradigm and sometimes "bortjaga" is its own paradigm
-        assert len(new_paradigms) == 551 or len(new_paradigms) == 552
+    final_tables = []
+    assert len(test_tables) == 9943
+    for table in test_tables:
+        msds_ = table["msds"]
+        tags = [("msd", msd) for msd in msds_]
+        final_tables.append((table["wordforms"], tags))
+    new_paradigms = learnparadigms(final_tables)
+    # this value is transient, sometimes 551 sometimes 552 (for full testset)
+    # it differs for the words "bortgallra", "bortkollra", "bortjaga", "bortsopa"
+    # where they sometimes are all in the same paradigm and sometimes "bortjaga" is its own paradigm
+    assert len(new_paradigms) in {551, 552}
